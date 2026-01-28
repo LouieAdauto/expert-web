7d22795d325b297388a61800862adb639824e854
Louie Adauto <fadauto@supplychainilluminations.com>
2026-01-28 12:24:43 -0600

    feat(ads): add google ads to results page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
+import Script, { ScriptProps } from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ApolloProviderWrapper from '@/providers/ApolloProviderWrapper';
+import ApolloProviderWrapper from "@/providers/ApolloProviderWrapper";
 import ThemeProvider from "@/components/ThemeProvider";
 import Providers from "@/providers/providers";
 
@@ -28,15 +29,23 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Mina:wght@400;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mina:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
           {children}
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2750583274550813"
+            crossOrigin="anonymous"
+          />
         </Providers>
       </body>
     </html>
